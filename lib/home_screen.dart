25bc0d324b33496d017b72f0121126378c8d6513
Miss_Sabrin <sabrincumar008@gmail.com>
2024-02-08 10:55:41 +0300

    api first

---
+++ lib/home_screen.dart
@@ -0,0 +1,82 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  //todo:
+  List<dynamic> users =[];
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('sipme api email and name')),
+      ),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context,index){
+        final user=users[index];
+        final email=user['email'];
+        final name=user['name']['first'];
+       final imageUrl=user['picture']['thumbnail'];
+
+
+        return ListTile(
+          leading: ClipRRect(
+            borderRadius: BorderRadius.circular(100),
+            child: Image.network(imageUrl),
+            
+            ),
+          title: Text(name),
+         subtitle: Text(email),
+
+          
+        );
+
+      }),
+
+
+
+
+
+
+
+
+
+      floatingActionButton: 
+      FloatingActionButton(
+        onPressed: fetchUsers,
+      
+      ),
+    );
+  }
+
+  
+void fetchUsers() async{
+  
+  print('fetchuser called');
+  const url='https://randomuser.me/api/?results=100';
+
+  final uri=Uri.parse(url);
+ final response=  await http.get(uri);
+ final body=response.body;
+ final json= jsonDecode( body);
+ setState((){
+
+   users =json['results'];
+
+ });
+ print('fetch completed');
+}
+}
